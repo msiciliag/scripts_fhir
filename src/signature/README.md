d447d799a68506c2a61971f2b87fabea014b3b28
Miguel Ángel Sicilia GarcÍa <m.sicilia.garcia@alumnos.upm.es>
2024-09-27 10:18:31 +0200

    Improved signature and documentation

--- src/signature/README.md
+++ src/signature/README.md
@@ -4,7 +4,7 @@ This directory contains scripts for signing and verifying FHIR resources.
 ## Usage
 
 ### Key generation:
-Run the following commands to generate a private key, a public key, and a certificate:
+Run the following commands in the same directory as the scripts to generate a private key, a public key, and a certificate:
 ```bash
 openssl req -x509 -newkey rsa:4096 -nodes -keyout private_key.pem -out certificate.pem -days 365 -subj "/C=US/ST=California/L=San Francisco/O=MyOrg/CN=JohnDoe"
 openssl x509 -in certificate.pem -pubkey -noout > public_key.pem
--- src/signature/sign.py
+++ src/signature/sign.py
@@ -2,12 +2,11 @@
 import json
 import base64
 from jose import jws
+from datetime import datetime, timezone
 
-# Load the FHIR resource
 with open('patient.json', encoding='utf-8') as f:
     fhir_resource = json.load(f)
 
-# Load the private key
 with open('private_key.pem', 'rb') as f:
     private_key = f.read()
 
@@ -15,42 +14,28 @@ jws_compact = jws.sign(
     fhir_resource,
     private_key,
     algorithm='RS256',
-    headers={'kid': 'my_key_id'}  # Optional: Key ID for identifying the signing key
 )
 
-# Create the Provenance resource
+now_utc = datetime.now(timezone.utc)
+patient_id = fhir_resource['id']
+
 provenance_resource = {
     "resourceType": "Provenance",
     "target": [
-        {"reference": "Patient/example"}
+        {"reference": "Patient/" + patient_id}
     ],
-    "recorded": "2024-11-16T12:00:00Z",  # Use current timestamp
+    "recorded": now_utc.strftime("%Y-%m-%dT%H:%M:%SZ"),
     "activity": {
         "coding": [
             {"system": "http://terminology.hl7.org/CodeSystem/v3-DocumentCompletion", "code": "LA"}
         ]
     },
-    "agent": [
-        {
-            "type": {
-                "coding": [
-                    {"system": "http://terminology.hl7.org/CodeSystem/provenance-participant-type", "code": "author"}
-                ]
-            },
-            "who": {
-                "reference": "Practitioner/your_practitioner_id"  #Replace with your practitioner ID
-            }
-        }
-    ],
     "signature": [
         {
             "type": [
                 {"system": "urn:iso-astm:E1762-95:2013", "code": "1.2.840.10065.1.12.1.1"}
             ],
-            "when": "2024-11-16T12:00:00Z",  # Use the same timestamp
-            "who": {
-                "reference": "Practitioner/your_practitioner_id"  #Replace with your practitioner ID
-            },
+            "when": now_utc.strftime("%Y-%m-%dT%H:%M:%SZ"),
             "sigFormat": "application/jose",
             "data": jws_compact
         }
